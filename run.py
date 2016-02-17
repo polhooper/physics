e66f55f382bfb62158478193b32a7ed390c34fec
apolhamus <aaronpolhamus@gmail.com>
2016-02-17 00:35:36 -0800

    double number of topic words displayed to 60

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ cmd = 'python term_doc_mat.py . 100'
 subprocess.call(cmd, shell=True) 
 
 print('Running the LDA analysis...')
-cmd = 'python topic_lda.py . 10 250 30'
+cmd = 'python topic_lda.py . 10 250 60'
 subprocess.call(cmd, shell=True) 
 
 print('Formatting data to Gephi-ingestible format...')
