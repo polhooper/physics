661574b023cd36e7f0263a2c27fa69bcf6e6789f
apolhamus <aaronpolhamus@gmail.com>
2016-02-17 01:13:26 -0800

    only 200 iterations are necessary before the LDA more or less converges

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ subprocess.call(cmd, shell=True)
 
 print('Running the LDA analysis...')
 #..30 - 50 branches of physics as the prior: https://en.wikipedia.org/wiki/Outline_of_physics
-cmd = 'python topic_lda.py . 50 500 60'
+cmd = 'python topic_lda.py . 50 200 60'
 subprocess.call(cmd, shell=True) 
 
 print('Formatting data to Gephi-ingestible format...')
