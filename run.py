8c65d9fb63415af96fd8443ffc6094627d32641b
apolhamus <aaronpolhamus@gmail.com>
2016-02-17 01:03:09 -0800

    increase number of topics in LDA to 50 per https://en.wikipedia.org/wiki/Outline_of_physics and double N iterations to 500

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ cmd = 'python term_doc_mat.py . 100'
 subprocess.call(cmd, shell=True) 
 
 print('Running the LDA analysis...')
-cmd = 'python topic_lda.py . 10 250 60'
+#..30 - 50 branches of physics as the prior: https://en.wikipedia.org/wiki/Outline_of_physics
+cmd = 'python topic_lda.py . 50 500 60'
 subprocess.call(cmd, shell=True) 
 
 print('Formatting data to Gephi-ingestible format...')
