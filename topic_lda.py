b7d58eb6f76a7d9f755939e6dad89a9015169de8
apolhamus <aaronpolhamus@gmail.com>
2016-02-16 23:55:05 -0800

    update so that top_words.txt is produced, which shows the top words within each topic category.

--- topic_lda.py
+++ topic_lda.py
@@ -1,4 +1,4 @@
-#..thanks to https://pypi.python.org/pypi/lda and 
+#..thanks to https://pypi.python.org/pypi/lda  
 
 import numpy as np 
 import pandas as pd 
@@ -8,6 +8,7 @@ import sys
 work_dir = sys.argv[1]
 n_topics = int(sys.argv[2])
 n_iter = int(sys.argv[3])
+n_top_words = int(sys.argv[4])
 
 def run_lda(): 
     term_doc = pd.read_csv('%s/tdm.csv' % work_dir)
@@ -20,6 +21,11 @@ def run_lda():
 
     topic_assignments = pd.DataFrame(model.doc_topic_)
     topic_assignments.to_csv('topic_weights.csv', index = False)
+
+    with open('top_words.txt', 'w') as f:         
+        for i, topic_dist in enumerate(topic_word):
+            topic_words = np.array(vocab)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
+            f.write('Topic {}: {}'.format(i, ' '.join(topic_words)) + '\n')
 
 if __name__ == '__main__':
     run_lda()
